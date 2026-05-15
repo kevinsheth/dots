6b99221b8626f23e6b7882fa7c63d1aece16d6d2
Kevin Sheth <kevinsheth25@gmail.com>
2026-05-15 11:13:54 -0400

    yes

--- shared/.config/fish/config.fish
+++ shared/.config/fish/config.fish
@@ -1,3 +1,9 @@
+set -gx JAVA_HOME /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home
+fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+fish_add_path /opt/homebrew/opt/openjdk@17/bin
+fish_add_path /opt/homebrew/share/google-cloud-sdk/bin
+fish_add_path ~/go/bin
+
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
